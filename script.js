6714b58ecbc09eaf24f2e5c3b31fe5afc28fc157
TeviBR99 <92033034+TeviBR99@users.noreply.github.com>
2024-12-16 23:41:49 +0100

    Main code pushed

---
+++ script.js
@@ -0,0 +1,176 @@
+
+const MAX_FILAS = 6;
+const MAX_COLUMNAS = 5;
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+const borrado = "⌫";
+const intro = "Enter"
+const diccionario = [ 
+    "apple", "table", "grape", "chair", "plane",
+    "water", "light", "night", "music", "stone",
+    "cloud", "green", "brick", "swirl", "watch",
+    "liver", "shoes", "vowel", "brush", "drink",
+    "piano", "lemon", "bingo", "flame", "scale",
+    "pearl", "jolly", "vivid", "sight", "clash",
+    "breeze", "flint", "frame", "skate", "reach",
+    "straw", "tease", "grape", "actor", "march",
+    "blaze", "lucky", "thick", "roast", "creek"
+];
+const secondsToDisplayDialog = 3;
+
+let word = "";
+let enterKeyPressed = false;
+let currentfilasIndex = 0;
+let currentColumnasIndex = 0;
+let lastElementInserted = 0;
+let gameWon;
+let gameLost;
+let lettersTyped = []
+
+function makeBoard(){
+    let tablero = "<table id='board'>"
+    for(let i=0; i<MAX_FILAS; i++){
+        tablero += "<tr class='row'>"
+        for(let j=0; j<MAX_COLUMNAS; j++){
+            tablero += "<td class='tile'></td>"
+        }
+        tablero += "</tr>"
+    }
+    tablero += "</table>"
+    document.getElementsByClassName('game-board')[0].innerHTML = tablero
+}
+
+function wordOfTheDay(){
+    const maxRandom = diccionario.length-1
+    word = diccionario[Math.floor(Math.random() * maxRandom)];
+}
+
+window.onload = function(){ 
+    gameWon = false;
+    gameLost = false;
+    makeBoard();
+    wordOfTheDay();
+    console.log("word: ", word)
+    document.querySelectorAll(".key").forEach((key) => key.addEventListener("click", (event) => writeOnBoard(event)))
+    document.getElementById('remove').addEventListener("click", () => removeOnBoard());
+    document.getElementById('enter').addEventListener("click", () =>{
+        checkIfExistWord()
+    });
+}
+
+function checkLineIsComplete(){
+    let cellFilled = 0
+    const row = getTiles(currentfilasIndex)
+    row.forEach((value, key) =>{
+        if(value && value.innerText !== ""){
+            cellFilled++
+        }
+    })
+    return (cellFilled === MAX_COLUMNAS);
+}
+
+function checkGameStatus(){
+    let correctLetter = 0;
+    for(let i=0; i<MAX_COLUMNAS; i++){
+        const tile = getTile(currentfilasIndex, i)
+        if(word.toUpperCase().includes(tile.innerText) && word.toUpperCase().charAt(i) === tile.innerText){
+            tile.classList.add('correct-letter')
+            correctLetter++;
+        }else if(word.toUpperCase().includes(tile.innerText) && word.toUpperCase().charAt(i) !== tile.innerText){
+            tile.classList.add('wrong-position')
+        }
+    }
+
+    if(correctLetter === word.length){
+        gameWon = true
+    }else if(correctLetter < word.length && currentfilasIndex === MAX_FILAS-1){
+        gameLost = true;
+    }
+}
+
+async function checkIfExistWord(){
+    const tiles = getTiles(currentfilasIndex)
+    let wordToCheck = "";
+    tiles.forEach((value, key) =>{
+        wordToCheck += value.innerText
+    })
+    const apiUrl = `https:api.dictionaryapi.dev/api/v2/entries/en/${wordToCheck}`
+    await fetch(apiUrl)
+        .then(response =>{
+            if(!response.ok){
+                const dialogMispelledWord = document.getElementById('dialog-mispelled-word')
+                dialogMispelledWord.showModal();
+                setTimeout(() => {
+                    dialogMispelledWord.close();
+                }, secondsToDisplayDialog * 1000);
+                currentfilasIndex = currentfilasIndex;
+                currentColumnasIndex = currentColumnasIndex;
+            }else{
+                nextLine()
+            }
+        }).catch(error =>{
+            console.log("Error: ", error)
+        })
+}
+
+function nextLine(){
+    if(currentColumnasIndex === MAX_COLUMNAS && currentfilasIndex < MAX_FILAS && checkLineIsComplete()){
+        checkGameStatus()
+        if(!gameWon && !gameLost){
+            currentfilasIndex++;
+            currentColumnasIndex = 0
+            enterKeyPressed = false;
+        }else if(gameWon){
+            const dialogYouWon = document.getElementById('dialog-you-won')
+            dialogYouWon.showModal()
+            document.getElementById('play-again-btn').addEventListener('click', () => {
+                dialogYouWon.close();
+                window.location.reload()
+            });
+        }else{
+            const dialogYouLost = document.getElementById('dialog-you-lost');
+            dialogYouLost.showModal()
+            document.getElementById('retry-btn').addEventListener('click', () => {
+                dialogYouLost.close();
+                window.location.reload()
+            });
+        }  
+    }
+}
+
+function writeOnBoard(element){
+    const keyValue = element.target.innerText;
+    const tile = getTile(currentfilasIndex, currentColumnasIndex)
+    if(tile && tile.innerText === "" && letters.includes(keyValue)){ 
+        tile.innerText = keyValue;
+        currentColumnasIndex++;
+        lastElementInserted = currentColumnasIndex-1;
+    }
+}
+
+function removeOnBoard(){
+    const tile = getTile(currentfilasIndex, lastElementInserted)
+    if(tile && tile.innerText){
+        tile.innerText = ""
+        currentColumnasIndex = currentColumnasIndex > 0 ? currentColumnasIndex - 1 : 0;
+        lastElementInserted = lastElementInserted > 0 ? lastElementInserted - 1 : 0;
+    }
+}
+
+function getTiles(rowIndex){
+    const rows = document.querySelectorAll(".row");
+    if(rows){
+        const row = rows[rowIndex];
+        if(row){
+            return row.querySelectorAll(".tile");
+        }
+    }
+    return null;
+}
+
+function getTile(rowIndex, columnIndex){
+    const tiles = getTiles(rowIndex);
+    if(tiles){
+        return tiles[columnIndex]   
+    }
+    return null;
+}
